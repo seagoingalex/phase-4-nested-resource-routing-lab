15028fbce9181864a2c6ad0a41dd52cdd426d07a
Alex Calvert <alexanderajcalvert@gmail.com>
2021-07-23 10:05:14 -0700

    commit tree dc3a33d7df4d787386b2f145a164b115c75f617a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+    else item = Item.create(item_params)
+    end
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
